34fd57d26368fa28c14def4229f10f622b9e1ac2
Akash Chauhan <92942033+akashchauhan21@users.noreply.github.com>
2024-10-26 19:22:50 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+
+    (function () {
+        var options = {
+            facebook: "580097572666574", // Facebook page ID
+            whatsapp: "+919693641958", // WhatsApp number
+            call_to_action: "Message us", // Call to action
+            button_color: "#FF6550", // Color of button
+            position: "left", // Position may be 'right' or 'left'
+            order: "facebook,whatsapp", // Order of buttons
+        };
+        var proto = 'https:', host = "getbutton.io", url = proto + '//static.' + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
